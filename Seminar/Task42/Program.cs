3448c468c3c8744ecc23dce3f90e3b8e1ab6383d
Andrey <a89615076005@yandex.ru>
2022-10-07 22:49:08 +0300

    add task 42

---
+++ Seminar/Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа, которая будет рекурсивно выводить каждый элемент массива в консоль
+Console.Clear();
+
+int[] array = CreateArray();
+FillArray(array);
+RecPrintArray(array);
+
+void RecPrintArray(int[] array, int index = 0)
+{
+    if (index < array.Length)
+    {
+        Console.Write($"{array[index]} ");
+        index++;
+        RecPrintArray(array, index);
+    }
+}
+
+
+int[] CreateArray()
+{
+    int[] array = new int [new Random().Next(4, 10)];
+    return array;
+}
+
+void FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(0, 11);
+    }
+} 
