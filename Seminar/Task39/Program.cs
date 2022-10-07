35bf3ed60f5d08ecff5f4dd596b508910b97d915
Andrey <a89615076005@yandex.ru>
2022-10-07 20:12:23 +0300

    add task 39

---
+++ Seminar/Task39/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход число А и выдает сумму чисел от 1 до А.
+//Использовать рекурсию.
+
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(RecSum(number));
+
+int RecSum(int number)
+{
+    if (number > 1)
+    {
+        return number + RecSum(number - 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
