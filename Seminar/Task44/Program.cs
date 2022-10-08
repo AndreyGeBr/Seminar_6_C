b397510538b6a8898f9161226d14b8cf7c0765a5
Andrey <a89615076005@yandex.ru>
2022-10-08 13:35:57 +0300

    add task44

---
+++ Seminar/Task44/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа, которая принимает на вход 2 числа, количество строк и количество столбцо, 
+// создаёт матрицу данного размера и заполняет нулями или единицами в случайном порядке и выводит в консоль.
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int row = Math.Abs(int.Parse(Console.ReadLine()!));
+Console.Write("Введите количество столбцов: ");
+int column = Math.Abs(int.Parse(Console.ReadLine()!));
+
+int[,] matrix = new int[row, column];
+FillArray(matrix);
+PrintArray(matrix);
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"| {matr[i, j]} |");
+        }
+Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 2);
+        }
+    }
+}
+
+
