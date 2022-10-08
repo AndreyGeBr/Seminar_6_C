739f1f009b3cbb21503dceb574b99347ccda8f7d
Andrey <a89615076005@yandex.ru>
2022-10-08 14:17:59 +0300

    add task 41

---
+++ Homework/Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите число M: "); 
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите M чисел через enter: ");
+int[] array = new int[M];
+int counter = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine()!);
+    if (array[i] > 0)
+    {
+        counter++;
+    }
+}
+ 
+Console.WriteLine($"Вы ввели {counter} чисел больше нуля");
